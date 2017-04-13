71c40d4a705d1d9f27db8c197f6a7b801e73cb19
Socha17 <mattsocha11@gmail.com>
2017-04-13 20:51:50 +0000

    added login/register page updated cookies handles errors

--- express_server.js
+++ express_server.js
@@ -3,8 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
+const serveStatic = require('serve-static');
 
-
+app.use(serveStatic(`${__dirname}/public`));
 app.use(cookieParser())
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,32 +16,94 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// how to send vars to partials, and make conditions of them
+const users = {
+    "userRandomID": {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur"
+    },
+   "user2RandomID": {
+     id: "user2RandomID",
+     email: "user2@example.com",
+     password: "dishwasher-funk"
+ }
+};
+
+
+let loginStatus = false
 
 app.get("/", (req, res) => {
   res.end("Hello!!! Welcome to Tiny Url");
 });
 
-app.post("/login", (req, res) => {
-  console.log('clicked log in');
-  console.log(req.body.username);
-  res.cookie('username', req.body.username);
+// register page
+app.get("/register", (req, res) => {
+  let templateVars = { urls: urlDatabase, allUsers: users, loginStatus };
+  res.render("urls_register", templateVars);
+});
+// register post
+app.post("/register", (req, res) => {
+  const checkEmails = checkRegisterEmails(req.body.email, users)
+  console.log('this is checkEmails' + checkEmails);
+    // add another if statment if email entered already exists return error
+  if (req.body.email == "") {
+    console.log('error 401');
+    res.statusCode = 401;
+    res.end("error 401 text field was blank")
+  }
+  if (checkEmails == false) {
+    res.statusCode = 401;
+    res.end("error 401 email already exists")
+  }
+
+  const createID = generateRandomString()
+  users[createID] = {
+    id: createID,
+    email: req.body.email,
+    password: req.body.password
+  };
+  loginStatus = true
+  // users[createID].email = req.body.email;
+  // users.createID.password = req.body.password;
+  console.log(users);
+  res.cookie('user_id', createID);
   res.redirect('/');
 });
-
+  // login get
+app.get("/login", (req, res) => {
+  console.log('clicked log in page');
+  let templateVars = { allUsers: users, loginStatus };
+  res.render('urls_login', templateVars);
+});
+// login post
+app.post("/login", (req, res) => {
+  console.log('clicked log in');
+  // check consts have value of the key from users object
+  const checkEmails = checkLoginEmail(req.body.email, users)
+  const checkPassword = checkPasswords(req.body.password, users)
+  if (checkEmails == "" && checkPassword == "" ) {
+    res.statusCode = 403;
+    res.end("error 403 make sure email and password are correct")
+  } else {
+    loginStatus = true
+    res.cookie('user_id', users[checkEmails].id);
+    res.redirect('/');
+  }
+});
+// logout post
 app.post("/logout", (req, res) => {
   console.log('clicked log out');
-  console.log(req.body.username);
-  res.clearCookie('username', req.body.username)
-  res.redirect('/urls');
+  res.clearCookie('user_id')
+  loginStatus = false;
+  res.redirect('/');
 });
-
+// index of urls
 app.get("/urls", (req, res) => {
   console.log('you are the the urls page');
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  let templateVars = { urls: urlDatabase, allUsers: users, loginStatus };
   res.render("urls_index", templateVars);
 });
-
+  // delete post
 app.post("/urls/:id/delete", (req, res) => {
   console.log('you deleted somthing ' + req.params.id);
   delete urlDatabase[req.params.id];
@@ -61,7 +124,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = {  username: req.cookies["username"] };
+  let templateVars = {  allUsers: users, loginStatus };
   res.render("urls_new", templateVars);
 });
 
@@ -73,7 +136,7 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
 
-  let templateVars = { shortURL: req.params.id, urls: urlDatabase[req.params.id], username: req.cookies["username"] };
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase[req.params.id], allUsers: users, loginStatus };
   res.render("urls_show", templateVars);
 });
 
@@ -97,4 +160,36 @@ function generateRandomString() {
   for( var i=0; i < 7; i++ )
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 return text;
+}
+
+
+let checkRegisterEmails = (email, objUsers) => {
+  let checkEmails = true;
+  Object.keys(objUsers).forEach(function (c, i) {
+      if (objUsers[c]['email'] == email) {
+        return checkEmails = false;
+      }
+  });
+      if (checkEmails == false) { return false; } else { return true; }
+}
+
+
+let checkLoginEmail = (email, objUsers) => {
+  let checkEmails = "";
+  Object.keys(objUsers).forEach(function (c, i) {
+      if (objUsers[c]['email'] == email) {
+        return checkEmails = c
+      }
+  });
+    return checkEmails;
+}
+
+let checkPasswords = (password, objUsers) => {
+  let checkPasswords = "";
+  Object.keys(objUsers).forEach(function (c, i) {
+      if (objUsers[c]['password'] == password) {
+        return checkPasswords = c;
+      }
+  });
+      return checkPasswords;
 }
